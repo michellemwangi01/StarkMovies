9323fbce621d644cbc0a85530bb523462ccd1a00
michellemwangi01 <michellemwangim@gmail.com>
2023-07-01 19:04:10 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    
-      fetchFilmData()
+  const movieImage = document.getElementById('movieImage')
+  const movieTitleDetails = document.getElementById('movieTitleDetails')
+  const movieDescription = document.getElementById('movieDescription')
+  const movierunTime = document.getElementById('movierunTime')
+  const movieShowTime = document.getElementById('movieShowTime')
+  const remainingTickets = document.getElementById('remainingTickets')
+  const movieTitlesContainer = document.getElementById('movieTitlesContainer')
+  const movieTitlesCard = document.createElement('div')
 
-})
 
-function fetchFilmData(){
-    fetch("http://localhost:3000/films")
-    .then((res)=> res.json())
-    .then(films => films.forEach(film => {
-        createMovieCard(film)
-        console.log(film.title);
+  document.addEventListener('DOMContentLoaded',()=>{
+    fetchFilmData()
+  })
+
+
+    function fetchFilmData() {
+      fetch("http://localhost:3000/films")
+        .then((res) => res.json())
+        .then((films) => {
+          let movieKeys = Object.keys(films);
+          let numberOfMovies = movieKeys.length;
+          let filmChoiceNum = Math.floor(Math.random() * numberOfMovies);
+          let filmChoice = films[movieKeys[filmChoiceNum]];
+          movieTitleDetails.textContent = filmChoice.title;
+          movieImage.src = filmChoice.poster;
+          movieDescription.textContent = filmChoice.description;
+          movierunTime.textContent = `RunTime: ${filmChoice.runtime} minutes`;
+          movieShowTime.textContent = `Showtime: ${filmChoice.showtime}`;
+          remainingTickets.textContent = `Remaining Tickets: ${
+            filmChoice.capacity - filmChoice.tickets_sold
+          }`;
+          console.log(films);
+            for(let film of films){
+              createMoviePreviewCard(film)
+            }
         
-    }));
-  }
+        });
+    }
 
-  function createMovieCard(film){
+
+  function createMovieDetailsCard(film){
     let cardContainer = document.createElement('div')
     cardContainer.setAttribute('class', 'container')
     let parentDiv = document.getElementById('bodyDiv')
@@ -32,8 +56,40 @@ function fetchFilmData(){
      <!--   <button class="btn fill">BUY NOW</button>-->
    </div>`
 
+  
+
 
   }
 
-//   ${film.poster}
+function createMoviePreviewCard(film){
+
+  movieTitlesContainer.append(movieTitlesCard)
+  movieTitlesCard.innerHTML = `
+    <div id="cardTitles">
+        <div class="imgPoster" id="imgPoster">
+            <img src="${film.poster}" alt="">
+        </div>
+        <div>
+            <h3 id="movieTitle">${film.title}</h3>
+        </div>
+        <div id="detailsBtnDiv">
+          <button id="movieDetails-${film.id}">Details</button>
+        </div>
+  </div>`
+
+    let movieDetailsBtn = document.getElementById(`movieDetails-${film.id}`)
+    movieDetailsBtn.addEventListener('click', ()=>{
+      
+       console.log(film.title);
+       movieTitleDetails.textContent = film.title
+       movieImage.src = film.poster
+       movieDescription.textContent = film.description
+       movierunTime.textContent = film.movierunTime
+       movieShowTime.textContent = film.showtime
+       remainingTickets.textContent = film.capacity - film.tickets_sold
+})
+
+
+}
+
 
